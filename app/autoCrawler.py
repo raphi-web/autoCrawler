d98aed15ce56d0a5b5bfcec1ab9aabd78d856ab7
raphi-web <raphael.prinz1993@gmail.com>
2022-03-21 19:50:16 +0100

    finalized project

--- app/autoCrawler.py
+++ app/autoCrawler.py
@@ -15,17 +15,20 @@ from selenium.common.exceptions import WebDriverException
 from flatten import booking2zip
 from geventwebsocket import WebSocketServer
 from engineio.async_drivers import gevent  # requirement for pyinstaller
-
-#from socket import socket
-# requirements for address extraction
 import requests
 from bs4 import BeautifulSoup
 from difflib import SequenceMatcher
 import asyncio
 
+
 # span[class="d543a27a3a"]
 # div[data-testid="map-trigger"]
 
+"""_summary_
+Collects Data from Booking.com via Request-querys that the website queries in the Database. 
+Returns:
+    _type_: _description_
+"""
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -33,16 +36,29 @@ app.config['SECRET_KEY'] = 'secret!'
 
 
 def update_page_status(message):
+    """_summary_
+    Args:
+        message (string): Message that gets send to the Control-Webside.
+    """
     socketio.emit('my_response', {'data': message})
 
-
 @socketio.on("connection")
-def send_conection_message(message):
+def send_conection_message(_):
+    """_summary_
+    Upon Connection the server prints that it is connected to the console.
+    This
+    Args:
+        _ (string): socketIO data but not needed 
+    """
     print("Connected to client!")
 
-
 @socketio.on("clear")
-def clear_folder(message):
+def clear_folder(_):
+    """_summary_
+    Deletes the collected raw-data from the BookingData folder
+    Args:
+        _ (_type_):  (string): socketIO data but not needed 
+    """
     print("Cleared!")
     try:
         for f in os.listdir("bookingData"):
@@ -53,6 +69,12 @@ def clear_folder(message):
 
 @socketio.on("mkfile")
 def files2zip(_):
+    """_summary_
+    Initializes the processing of raw data to csv/geoson
+    Sends message to client-js that download is ready
+    Args:
+        _ (string): socketIO data but not needed 
+    """
     try:
         print("Prepering Data")
         booking2zip()
@@ -67,7 +89,12 @@ def files2zip(_):
             socketio.emit('download_ready', {'data': "download_ready"})
 
 @socketio.on("exit")
-def exit_browser(message):
+def exit_browser(_):
+    """_summary_
+    Closes browser and quits programm
+    Args:
+       _ (string): socketIO data but not needed 
+    """
     try:
         driver.quit()
         sys.exit()
@@ -77,10 +104,21 @@ def exit_browser(message):
 
 @app.route("/")
 def root():
+    """_summary_
+    Routes the client to the control-webside
+    Returns:
+        _type_: html-file
+    """
     return render_template("index.html")
 
 @app.route("/start", methods=["POST"])
 def start_collecting():
+    """_summary_
+    Initializes collection of Hotels, receive data from client form and
+    send it to collecting function in new thread
+    Returns:
+        _type_: _description_
+    """
     nxt_pg_btn = request.form["nxt_pg_btn"]
     map_btn = request.form["map_btn"]
     close_map_btn = request.form["close_map_btn"]
@@ -94,11 +132,15 @@ def start_collecting():
     threading.Thread(target=booking_collect, args=(
         nxt_pg_btn, map_btn, close_map_btn, json_name, address_check)).start()
 
+    update_page_status("Started collecting, this may takes time to start up...")
     return "ok"
 
 
 @app.route('/get-files/booking_data.zip', methods=['GET', 'POST'])
 def get_files():
+    """_summary_
+    Routes to the collected data-file
+    """
     path = os.getcwd()
     file = path + "/bookingHotels/booking_data.zip"
     try:
@@ -109,6 +151,9 @@ def get_files():
 
 
 def start_server():
+    """_summary_
+    Starts the server on Port 5000
+    """
     host = "127.0.0.1"
     port = 5000
     http_server = WebSocketServer((host, port), app)
@@ -116,6 +161,15 @@ def start_server():
 
 
 def booking_collect(nxt_pg_btn, map_btn, close_map_btn, json_name, address_check):
+    """_summary_
+    Iterates over the pages of the booking page and collects the data 
+    Args:
+        nxt_pg_btn (string): name of the html-element for the next page
+        map_btn (string): name of the html-element to open the map
+        close_map_btn (string): html-element to close map 
+        json_name (string): name of the json file 
+        address_check (bool): if true adresses are collected too  
+    """
     page = 0
     try:
         next_page_btn = driver.find_element(By.CSS_SELECTOR, nxt_pg_btn)
@@ -135,6 +189,8 @@ def booking_collect(nxt_pg_btn, map_btn, close_map_btn, json_name, address_check
                 hotel_response = None
                 max_wait = 30
                 second_counter = 0
+                
+                # try to listen for json data file in requests
                 while hotel_response == None and second_counter < max_wait:
                     time.sleep(1)
                     second_counter += 1
@@ -143,12 +199,13 @@ def booking_collect(nxt_pg_btn, map_btn, close_map_btn, json_name, address_check
 
                     if len(filterd_requests) > 0:
                         hotel_response = filterd_requests[0].response
-
+                # decompress the data
                 response_json = brotli.decompress(
                     hotel_response.body).decode("utf-8")
                 response_json = json.loads(response_json)
 
-                if address_check:
+                # insert addresses to the hotel data json file
+                if address_check: 
                     for idx, hotel in enumerate(response_json["b_hotels"]):
                         hotel_name = hotel["b_hotel_title"]
                         for addr_idx, hotel_addr_name in enumerate(hotel_addr_keys):
@@ -185,6 +242,12 @@ def booking_collect(nxt_pg_btn, map_btn, close_map_btn, json_name, address_check
 
 
 async def get_addresses():
+    """_summary_
+    Iterates over all anchor-tags in the booking site and opens the links to the hotels
+    to extract the addresses out of them 
+    
+    returns (dict): string:string
+    """
     async def request_hotels(link):
         try:
             hotel_soup = BeautifulSoup(requests.get(
@@ -221,8 +284,18 @@ async def get_addresses():
 
 
 def similar(a, b):
+    """_summary_
+    Computes the similarity of two strings
+    Args:
+        a (string): string a
+        b (string): string b
+
+    Returns:
+        float: how much both strings match together as ratio
+    """
     return SequenceMatcher(None, a, b).ratio()
 
+################################ Init Main Loop ################################
 
 print("Copy Geckodriver to this Folder:\n", os.getcwd(), "\n\n")
 
@@ -244,16 +317,14 @@ driver = webdriver.Firefox(executable_path=driver_name)
 th = threading.Thread(target=start_server)
 th.start()
 
-update_page_status("Logs will be displayed here!")
-
 driver.get("http://127.0.0.1:5000/")
 driver.execute_script("window.open()")
 
 tabs = driver.window_handles
 driver.switch_to.window(tabs[-1])
 
-
 driver.get('https://www.booking.com/')
 time.sleep(1)
 driver.switch_to.window(tabs[0])
+update_page_status("Connected to Server, logs will be displayed here!")
 th.join()
